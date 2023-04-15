59033f6827173c7cccd7ee7c9de8fe3031ffd4f4
Adesh <adeshmutadak@gmail.com>
2023-04-15 23:15:55 +0530

    This is Code For Mail Sending Using JAVA.

--- SendMailJava/src/main/java/org/geekster/MailConstants.java
+++ SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -2,14 +2,15 @@ package org.geekster;
 
 public class MailConstants {
 
-    public static final String SENDER = "mainakgh1@gmail.com";
+    public static final String SENDER = "adeshmutadak@gmail.com";
 
     // receiver email
-    public static final String RECEIVERMAILADDRESS = "dpbehera1997@gmail.com";
+    public static final String RECEIVERMAILADDRESS = "adeshmutadak123@gmail.com";
 
     //subject of the mail
     public static final String SUBJECT = "Testing Java mail : 12-April-2023";
 
     //message to be sent in the mail body
-    public static final String MESSAGE = "This message is for security check...!!!";
+   public static final String MESSAGE = "This message is for security check...!!!";
+    //public static final String MESSAGE="RISHI Ani SAyukta NIBBa Ani NIBBI";
 }
